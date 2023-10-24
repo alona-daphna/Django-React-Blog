1e7f6f09ab093f9daa364322c2503b7f38cccb7c
alona <alona.daphna@gmail.com>
2023-10-24 10:29:07 +0300

    added styles to admin login page

--- blog-frontend/src/views/AdminLogin.tsx
+++ blog-frontend/src/views/AdminLogin.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const AdminLogin = () => {
-  return <div>AdminLogin</div>;
+  const navigate = useNavigate();
+  const handleLogin = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+
+    // if secret valid
+    if (true) {
+      // set token in local storage
+      navigate('/');
+    }
+  };
+
+  return (
+    <div className="admin-login">
+      <h1>Admin</h1>
+      <form>
+        <input type="text" placeholder="secret" />
+        <button onClick={handleLogin}>Login</button>
+      </form>
+    </div>
+  );
 };
